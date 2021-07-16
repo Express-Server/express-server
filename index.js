b247681890ccfe87bbbc51c7682f59511a3d6cc7
Tomas Peterec <info@tomaspeterec.sk>
2021-07-17 00:32:47 +0200

    Fixed code styling

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ app.post('/login', login.loginFormSubmit)
 app.get('/logout', login.renderLogout)
 
 const renderMessages = (req, res) => {
-    console.log("session", req.session)
-    if (req.session.user) {
-    knex("messages")
-        .join("users", "messages.user_id", "=", "users.id")
-        .orderBy('timestamp', 'desc')
-        .then(messages =>
-            res.send(`
+  console.log('session', req.session)
+  if (req.session.user) {
+    knex('messages')
+      .join('users', 'messages.user_id', '=', 'users.id')
+      .orderBy('timestamp', 'desc')
+      .then(messages =>
+        res.send(`
             <h1>${req.session.user.name} Vitaj!</h1>
             <a href="/logout" >odhlas sa</a>
                 <h1>Zatiaľ napíš čo sťeš</h1>
@@ -43,45 +43,45 @@ const renderMessages = (req, res) => {
                     <input type="submit"/>
                     <ul>
                         ${messages
-                            .map(message => `<li>${message.timestamp} [${message.name}]: ${message.text}</li>`)
-                            .join("")}
+    .map(message => `<li>${message.timestamp} [${message.name}]: ${message.text}</li>`)
+    .join('')}
                     </ul>
                 </form>
                 `))} else {
-                    res.send(`
+    res.send(`
                         <h1>Nazdar neni si prihlaseny!/prihlasena!</h1>
                         <a href="/login" >Prihlas sa</a>
                         <div>alebo sa</div>
                         <a href="/signup">zaregistruj</a>.
                     `)
-                }
+  }
             
 }
 
-app.get("/", renderMessages)
+app.get('/', renderMessages)
 
 
 
 
 
-app.post("/", (req, res) => {
-    knex("users")
-        .where({name: req.session.user.name})
-        .first()
-        .then(user => {
-            if (!user) {
-                knex("users")
-                    .insert({name: req.body.userName})
-                    .then(id => { saveMessage(id, req.body.message)})
-                    .then(() => { res.redirect(302, "/")})
-            } else
-                saveMessage(user.id, req.body.message)
-                    .then(() => res.redirect(302, "/"))
-        })
+app.post('/', (req, res) => {
+  knex('users')
+    .where({name: req.session.user.name})
+    .first()
+    .then(user => {
+      if (!user) {
+        knex('users')
+          .insert({name: req.body.userName})
+          .then(id => { saveMessage(id, req.body.message)})
+          .then(() => { res.redirect(302, '/')})
+      } else
+        saveMessage(user.id, req.body.message)
+          .then(() => res.redirect(302, '/'))
+    })
 })
 
 
 
 
-app.listen(8080, "0.0.0.0") 
-console.log("listeing on http://localhost:8080")
+app.listen(8080, '0.0.0.0') 
+console.log('listeing on http://localhost:8080')
